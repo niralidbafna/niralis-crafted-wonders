8098ba4ea2826199613fd42cc4d34ec904e4d8f2
nirali <niralideepakbafna@gmail.com>
2026-04-14 02:38:10 +0530

    fix: use relative base path with HashRouter

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/niralis-crafted-wonders/',
+  base: './',
   plugins: [react()],
   server: {
     port: 3000,
